064b514e19514c50672313de4e8c25ca319fc96b
chrispsheehan <chris_sheehan@hotmail.co.uk>
2020-08-20 20:26:35 +0100

    Removed steps - will redo

--- src/Steps/Steps.cs
+++ src/Steps/Steps.cs
@@ -13,59 +13,6 @@ namespace StepDefinitions
         {
             _driver = driver;
         }
-
-        [Given(@"The profanity removing API is available")]
-        public void GivenTheProfanityRemovingAPIIsAvailable()
-        {
-
-        }
-        
-        [When(@"I receive an incomming text of (.*)")]
-        public void WhenIReceiveAnIncommingTextOf(string text)
-        {
-            Console.WriteLine(text);
-        }
-        
-        [When(@"run it through the profanity API")]
-        public void WhenRunItThroughTheProfanityAPI()
-        {
-
-        }
-        
-        [Then(@"no profanitys are removed")]
-        public void ThenNoProfanitysAreRemoved()
-        {
-
-        }
-        
-        [Then(@"no asterisks are added")]
-        public void ThenNoAsterisksAreAdded()
-        {
-
-        }
-        
-        [Then(@"profanitys are removed")]
-        public void ThenProfanitysAreRemoved()
-        {
-
-        }
-        
-        [Then(@"replaced with the corresponding amount of asterisks")]
-        public void ThenReplacedWithTheCorrespondingAmountOfAsterisks()
-        {
-
-        }
-
-        [Then(@"profanitys are found")]
-        public void ThenProfanitysAreFound()
-        {
-
-        }
-
-        [Then(@"no profanitys are found")]
-        public void ThenNoProfanitysAreFound()
-        {
-
-        }                                
+                               
     }
 }
