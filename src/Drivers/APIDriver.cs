8fd2015a03d3ba2ad90b3c4098c04b6c0cca3b11
chrispsheehan <chris_sheehan@hotmail.co.uk>
2020-08-20 21:46:01 +0100

    base functions for API usage

--- src/Drivers/APIDriver.cs
+++ src/Drivers/APIDriver.cs
@@ -1,13 +1,45 @@
 using System;
+using System.Net;
+using RestSharp;
 
 namespace Drivers
 {
     public class APIDriver
     {
+        private readonly RestClient _client;
+
+        private RestRequest _restRequest;
+
         public APIDriver()
         {
-            
+            _client = new RestClient
+            {
+                BaseUrl = new Uri("https://www.purgomalum.com")
+            };
         }
 
+        public void SetService(string serviceName)
+        {
+            _restRequest = new RestRequest
+            {
+                Resource = $"Service/{serviceName}"
+            };
+        }
+
+        private bool RemoteFileExists(string url)
+        {
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(_client.BaseUrl) as HttpWebRequest;
+                request.Method = "HEAD";
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                response.Close();
+                return (response.StatusCode == HttpStatusCode.OK);
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
